1c26b8e547163fa19561602328ec536f6f909866
dhruvk <dhruv21.dk@gmail.com>
2023-03-20 17:23:53 +0530

    hi

--- server/index.js
+++ server/index.js
@@ -9,6 +9,10 @@ app.use(cors());
 app.use(express.json({ limit: "50mb" }));
 
 require("dotenv").config();
+app.get("/", (req, res) => {
+  res.send({ message: "Hello World!" });
+});
+
 app.use("/api/v1/users", userRoutes);
 app.use("/api/v1/properties", propertyRoutes);
 mongoose.connect(process.env.ATLAS_URL,{
